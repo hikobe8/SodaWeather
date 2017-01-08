8fb9669202befda05cc2642205a2d67ba89bee2c
hikobe8 <hikoeb8@163.com>
2017-01-08 16:22:12 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/soda/sodaweather/db/County.java
@@ -0,0 +1,48 @@
+package com.soda.sodaweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Ray on 2017/1/8.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
