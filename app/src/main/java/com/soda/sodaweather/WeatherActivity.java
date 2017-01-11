381937ca059f8d40136edfd1333e8ad9a188ea04
hikobe8 <hikoeb8@163.com>
2017-01-12 00:44:39 +0800

    已选择城市的自动定位

--- app/src/main/java/com/soda/sodaweather/WeatherActivity.java
+++ app/src/main/java/com/soda/sodaweather/WeatherActivity.java
@@ -1,15 +1,22 @@
 package com.soda.sodaweather;
 
-import android.app.Fragment;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,15 +26,25 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.soda.sodaweather.db.City;
+import com.soda.sodaweather.db.County;
 import com.soda.sodaweather.gson.Forecast;
 import com.soda.sodaweather.gson.Weather;
 import com.soda.sodaweather.service.AutoUpdateService;
 import com.soda.sodaweather.util.HttpUtil;
 import com.soda.sodaweather.util.Utility;
 
+import org.litepal.crud.DataSupport;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -75,9 +92,15 @@ public class WeatherActivity extends AppCompatActivity {
     ChooseAreaFragment mChooseAreaFragment;
 
     private String mWeatherId;
+
+    //定位
+    private LocationClient mLocationClient;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
    /*     if (Build.VERSION.SDK_INT >= 21) {
             //大于5.0 设置状态栏透明
             View decorView = getWindow().getDecorView();
@@ -86,6 +109,10 @@ public class WeatherActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }*/ //TODO 5.0 状态栏白色
         setContentView(R.layout.activity_weather);
+        checkNeedPermissoion();
+    }
+
+    private void loadWeather(){
         ButterKnife.bind(this);
         mChooseAreaFragment = (ChooseAreaFragment) getSupportFragmentManager().findFragmentById(R.id.choose_area_fragment);
         mSwipeRefresh.setColorSchemeResources(R.color.colorPrimary);
@@ -132,6 +159,51 @@ public class WeatherActivity extends AppCompatActivity {
         }
         Intent intent = new Intent(this, AutoUpdateService.class);
         startService(intent);
+    }
+
+
+    /**
+     * 检查所需权限
+     */
+    private void checkNeedPermissoion() {
+        List<String> permissionList = new ArrayList<>();
+        //访问位置的权限
+        if (ContextCompat.checkSelfPermission(this, Manifest
+                .permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest
+                .permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        //读取手机状态
+        if (ContextCompat.checkSelfPermission(this, Manifest
+                .permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        //读写手机外部存储权限
+        if (ContextCompat.checkSelfPermission(this, Manifest
+                .permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        } else {
+            //权限通过
+            requestLocation();
+            loadWeather();
+        }
+    }
+
+    private void requestLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        option.setIsNeedAddress(true);
+        mLocationClient.setLocOption(option);
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        mLocationClient.start();
     }
 
     /**
@@ -246,6 +318,112 @@ public class WeatherActivity extends AppCompatActivity {
             }
         } else {
             super.onBackPressed();
+        }
+    }
+
+    private class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            String currentLocationDistrict = bdLocation.getDistrict();
+            checkCityExist(currentLocationDistrict);
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition
+                    .append("纬度: ")
+                    .append(bdLocation.getLatitude())
+                    .append("\n")
+                    .append("经度: ")
+                    .append(bdLocation.getLongitude())
+                    .append("\n")
+                    .append("国家: ")
+                    .append(bdLocation.getCountry())
+                    .append("\n")
+                    .append("省:")
+                    .append(bdLocation.getProvince())
+                    .append("\n")
+                    .append("市:")
+                    .append(bdLocation.getCity())
+                    .append("\n")
+                    .append("区: ")
+                    .append(bdLocation.getDistrict())
+                    .append("\n")
+                    .append("街道: ")
+                    .append(bdLocation.getStreet())
+                    .append("\n")
+                    .append("定位方式: ");
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
+                currentPosition.append("GPS");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                currentPosition.append("网络");
+            }
+            Log.e("location", currentPosition.toString());
+//            Toast.makeText(WeatherActivity.this, currentPosition.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * 检查当前城市是否存在城市数据库
+     * 存在提示用户是否选择显示当前定位城市的天气
+     */
+    private void checkCityExist(String district) {
+        //egg pain 删除最后一个字来模糊查询
+        district = district.substring(0, district.length() - 1);
+        /**
+         * 进入weather详细页面必定已经缓存了所有省市
+         */
+        County foundCounty = DataSupport.where("countyName = ?", district)
+                .findFirst(County.class);
+        if (foundCounty != null) {
+            //城市存在 提示是否切换当前城市
+            showSwitchCityDialog(foundCounty);
+        }
+
+    }
+
+    /**
+     * 切换城市的对话框
+     * @param county 当前定位的城市
+     */
+    private void showSwitchCityDialog(final County county) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name)
+                .setMessage("当前定位城市为 "+ county.getCountyName() +",\n" +"是否切换")
+                .setPositiveButton("切换", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mWeatherId = county.getWeatherId();
+                        requestWeather(mWeatherId);
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i ++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "权限未通过"+permissions[i], Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    requestLocation();
+                    loadWeather();
+                } else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
         }
     }
 }
